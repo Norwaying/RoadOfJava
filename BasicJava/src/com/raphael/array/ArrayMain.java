1c1f01e9c7c0dc551d8df8df0644fac909a31c07
Norwaying <norwaying@gmail.com>
2015-01-06 17:34:24 +0800

    test another branch

--- BasicJava/src/com/raphael/array/ArrayMain.java
+++ BasicJava/src/com/raphael/array/ArrayMain.java
@@ -3,6 +3,7 @@ package com.raphael.array;
 /**
  * Created by Nov on 2015/1/6.
  * Java数组使用
+ *
  */
 public class ArrayMain {
 }
--- BasicJava/src/com/raphael/construct/ConstructMain.java
+++ BasicJava/src/com/raphael/construct/ConstructMain.java
@@ -2,7 +2,7 @@ package com.raphael.construct;
 
 /**
  * Created by Nov on 2015/1/6.
- * Java构造函数及构造块
+ * Java默认构造函数,构造函数,带参构造函数及构造块
  */
 public class ConstructMain {
 }
